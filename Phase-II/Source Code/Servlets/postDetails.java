9c1fbec0f108420a6f998ccacf1cc3dd34f45ba3
yagantiashok@gmail.com <yagantiashok@gmail.com>
2016-07-07 21:09:27 -0500

    Added Google search and statistics code

---
+++ Phase-II/Source Code/Servlets/postDetails.java
@@ -0,0 +1,73 @@
+package BloggerSourceCode;
+
+public class postDetails {
+	
+	private String postid11;
+	private String post1;
+	private String title;
+	private String name;
+	private String email;
+	private String date1;
+	
+	 
+	 public String Postid11()
+	  {
+	  return this.postid11;
+	  }	
+     public void Postid11(String postid11)
+	  {
+	     this.postid11 = postid11;
+	  }
+     
+	
+	  
+	  public String Post1()
+	  {
+	  return this.post1;
+	  }	
+      public void Post1(String post1)
+	  {
+	     this.post1 = post1;
+	  }
+      
+      
+      public String Title()
+	  {
+	  return this.title;
+	  }	
+      public void Title(String title)
+	  {
+	     this.title = title;
+	  }
+			  
+     
+      public String Name()
+ 	  {
+ 	  return this.name;
+ 	  } 	
+      public void Name(String name)
+ 	  {
+ 	     this.name = name;
+ 	  }
+      
+      
+      public String Email()
+  	  {
+  	  return this.email;
+  	  }  	
+      public void Email(String email)
+  	  {
+  	     this.email = email;
+  	  }
+      
+      
+      public String Date1()
+  	  {
+  	  return this.date1;
+  	  }  	
+      public void Date1(String date1)
+  	  {
+  	     this.date1 = date1;
+  	  }
+
+}
